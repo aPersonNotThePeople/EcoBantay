e8cffed529ec672877438478a9b9e41f016b432e
Nekosan589 <alex.turano589@gmail.com>
2026-03-16 07:51:16 +0800

    fix: modified solenoid opening lock to be more concise

--- Arduino-R4-Repository/Sorting-Module/solenoid-lock.cpp
+++ Arduino-R4-Repository/Sorting-Module/solenoid-lock.cpp
@@ -6,15 +6,8 @@ void initSolenoidLock() {
 }
 
 // open duration is in milliseconds
-void openSolenoidLock(int solenoid_lock_number, float open_duration){
-    switch (solenoid_lock_number) {
-        case 1: 
-            digitalWrite(SOLENOID_LOCK1_RELAY, HIGH); //opens
-            delay(constrain(open_duration, 500, MAXIMUM_OPEN_DURATION));
-            (SOLENOID_LOCK1_RELAY, LOW); //closes
-        case 2:
-            digitalWrite(SOLENOID_LOCK2_RELAY, HIGH); //opens
-            delay(constrain(open_duration, 500, MAXIMUM_OPEN_DURATION));
-            (SOLENOID_LOCK2_RELAY, LOW); //closes
-    }
+void openSolenoidLock(int solenoid_pin_number, float open_duration){
+    digitalWrite(solenoid_pin_number, HIGH); //opens
+    delay(constrain(open_duration, 500, MAXIMUM_OPEN_DURATION));
+    digitalWrite(solenoid_pin_number, LOW); //closes
 }
